d1955c275bf6edfaa53c4b98ac48a2050730af0d
0x-stan <744428333@qq.com>
2022-11-25 12:58:41 +0800

    deploy script update

--- test/UpgradeableProxy.t.sol
+++ test/UpgradeableProxy.t.sol
@@ -44,41 +44,4 @@ contract UpgradeableProxyTest is ArtGobblersDeployHelper {
         assertTrue(oldVoltronAddr != newVoltronAddr);
     }
 
-    function testDepositGobblers() public {
-        uint256 gobblersNum = 2;
-
-        uint256[] memory gobblerIds = mintGobblers(users[0], gobblersNum);
-        vm.warp(block.timestamp + 1 days);
-        setRandomnessAndReveal(gobblersNum, "seed");
-
-        (uint32 userGobblersOwnedBefore, uint32 userEmissionMultipleBefore,,,,) = voltronProxy.getUserData(users[0]);
-
-        deposit(users[0], gobblerIds);
-
-        assertEq(gobblers.balanceOf(address(voltronProxy)), gobblersNum);
-        assertEq(gobblers.balanceOf(users[0]), 0);
-
-        uint32 sumEmissionMultiple;
-        for (uint256 i = 0; i < gobblersNum; i++) {
-            assertEq(gobblers.ownerOf(gobblerIds[i]), address(voltronProxy));
-            assertEq(voltronProxy.getUserByGobblerId(gobblerIds[i]), users[0]);
-            (,, uint32 emissionMultiple) = gobblers.getGobblerData(gobblerIds[i]);
-            sumEmissionMultiple += emissionMultiple;
-        }
-
-        (uint32 userGobblersOwnedAfter, uint32 userEmissionMultipleAfter,,,,) = voltronProxy.getUserData(users[0]);
-        assertEq(userGobblersOwnedBefore + gobblersNum, userGobblersOwnedAfter);
-        assertEq(userEmissionMultipleBefore + sumEmissionMultiple, userEmissionMultipleAfter);
-
-        (uint32 totalGobblersDeposited, uint32 totalEmissionMultiple,,) = voltronProxy.globalData();
-        assertEq(totalGobblersDeposited, gobblersNum);
-        assertEq(totalEmissionMultiple, sumEmissionMultiple);
-    }
-
-    function deposit(address user, uint256[] memory gobblerIds) internal {
-        vm.startPrank(user);
-        gobblers.setApprovalForAll(address(voltronProxy), true);
-        voltronProxy.depositGobblers(gobblerIds, 0);
-        vm.stopPrank();
-    }
 }
